24149aa4f067fc981b6b52a1a3f4eca96c7299a7
fds66 <fds66@outlook.com>
2026-03-30 23:34:54 +0100

    first server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module workspace/fds66/github.com/fds66/chirpy
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	serveMux := http.NewServeMux()
+
+	serveMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: serveMux,
+	}
+
+	fmt.Println("serving")
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
