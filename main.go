4ec6bb4c2f60fc762077338ae2133af91796836d
fds66 <fds66@outlook.com>
2026-03-31 15:17:55 +0100

    added another handler for ready status

--- main.go
+++ main.go
@@ -9,15 +9,26 @@ import (
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	//Initialise the Mux
 	serveMux := http.NewServeMux()
-
-	serveMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	//fileserver with built-in handler
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: serveMux,
 	}
+	//register our handler
+	serveMux.HandleFunc("/healthz", handlerReady)
 
 	fmt.Println("serving")
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+// handler that gives the ready response
+func handlerReady(w http.ResponseWriter, r *http.Request) {
+	//Header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
